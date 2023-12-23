9c711a592de7f26598a9bed96650a40566f850e3
Lalita Gupta <lgupta0731@gmail.com>
2023-12-23 17:31:02 +0530

    main.py

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ def rms(image):
 
 def table():
 
-    if choice4 == "HUE Coloration":
+    if choice4 == "HUE Coloration" or choice2 == "Canny Edge Detection" or choice2 == "Otsu Edge Detection":
         # dictionary of lists 
         dict = {"Mean": avg_total, "STD": std_total, "Var": var_total, "RMS": rms_total} # "MSE": mse_total
         df = pd.DataFrame(dict)
